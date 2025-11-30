cc525b9eae498fbe77d72f894e269b23fe8efd9b
greycat.dev <greycat.dev@gmail.com>
2025-11-30 22:54:00 +0530

    gg

--- server.js
+++ server.js
@@ -107,18 +107,7 @@ app.use(
   })
 );
 
-// Preflight
-// Preflight (fixed for Express/path-to-regexp v7)
-app.options("/*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  );
-  return res.sendStatus(200);
-});
+
 
 
 /* ---------------------------------------------------------
